ec4116ec0b0f481a358fbf27587a0fe179d1b06d
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-03 11:30:32 +0700

    feat: apply cors middleware in main routes to enable for all children routes

--- router/main.go
+++ router/main.go
@@ -1,7 +1,13 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"noir-backend/middleware"
+
+	"github.com/gin-gonic/gin"
+)
 
 func CombineRouter(r *gin.Engine) {
+	r.Use(middleware.CORS())
 	authRouter(r.Group("/auth"))
+	userRouter(r.Group("/user"))
 }
