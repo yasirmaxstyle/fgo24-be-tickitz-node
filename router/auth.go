8327f4a451f328f08fb65754ae270d78c21a8cd9
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-03 11:20:32 +0700

    feat: add controller for authentication path

--- router/auth.go
+++ router/auth.go
@@ -1,8 +1,14 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"noir-backend/controllers"
+
+	"github.com/gin-gonic/gin"
+)
 
 func authRouter(r *gin.RouterGroup) {
-	r.POST("/register")
-	r.POST("/login")
+	r.POST("/register", controllers.Register)
+	r.POST("/login", controllers.Login)
+	r.POST("/forgot-password", controllers.ForgotPassword)
+	r.POST("/reset-password", controllers.ResetPassword)
 }
