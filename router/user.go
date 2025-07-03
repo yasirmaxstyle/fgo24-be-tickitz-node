a5647bafe1f9a59f66f6f946a75c336b3248acd0
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-03 11:29:12 +0700

    feat: apply auth middleware for user path routes

--- router/user.go
+++ router/user.go
@@ -2,11 +2,13 @@ package router
 
 import (
 	"noir-backend/controllers"
+	"noir-backend/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func userRouter(r *gin.RouterGroup) {
+	r.Use(middleware.AuthMiddleware())
 	r.GET("/:id", controllers.GetProfile)
 	r.PATCH("/:id") //edit profile
 }
