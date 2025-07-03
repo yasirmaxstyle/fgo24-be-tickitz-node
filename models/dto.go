c72bf444f7a288bcd076d5448037ceab23b7831c
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-03 11:31:43 +0700

    feat: add model data for reset password request

--- models/dto.go
+++ models/dto.go
@@ -1,9 +1,11 @@
 package models
 
+import "github.com/gin-gonic/gin"
+
 type RegisterRequest struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=8"`
-	FullName string `json:"full_name" binding:"required"`
+	Email           string `form:"email" binding:"required,email"`
+	Password        string `form:"password" binding:"required,min=8"`
+	ConfirmPassword string `form:"confirmPassword" binding:"required"`
 }
 
 type LoginRequest struct {
@@ -25,4 +27,13 @@ type APIResponse struct {
 type AuthResponse struct {
 	User  *User  `json:"user"`
 	Token string `json:"token"`
+}
+
+type PasswordResetRequest struct {
+	Email string `json:"email" binding:"required,email"`
+}
+
+type ResetPasswordRequest struct {
+	Token       string `json:"token" binding:"required"`
+	NewPassword string `json:"new_password" binding:"required,min=6"`
 }
