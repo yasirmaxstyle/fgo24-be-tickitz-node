b5f810f12f64e3c41e331fe91e3a8785dffc2ce8
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-07 01:30:32 +0700

    feat: add swag docs, add admin routes, and apply middleware error handle for handling panics

--- router/main.go
+++ router/main.go
@@ -1,13 +1,20 @@
 package router
 
 import (
+	docs "noir-backend/docs"
 	"noir-backend/middleware"
 
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func CombineRouter(r *gin.Engine) {
+	docs.SwaggerInfo.BasePath = "/"
 	r.Use(middleware.CORS())
+	r.Use(middleware.ErrorHandler())
 	authRouter(r.Group("/auth"))
-	userRouter(r.Group("/user"))
+	adminRouter(r.Group("/admin"))
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
