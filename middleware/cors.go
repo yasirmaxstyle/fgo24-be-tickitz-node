a9600915e47cef16514d49f16a3d44aa436a32a7
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-07 01:22:52 +0700

    feat: add patch method in cors method

--- middleware/cors.go
+++ middleware/cors.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 		if c.Request.Method == "OPTIONS" {
