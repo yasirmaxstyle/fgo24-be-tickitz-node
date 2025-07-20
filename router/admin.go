bcc04e98702c1b0620fc63a0491b92e1ee1a1dc5
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-20 19:01:35 +0700

    feat: add miggleware admin role=admin

--- router/admin.go
+++ router/admin.go
@@ -2,11 +2,13 @@ package router
 
 import (
 	"noir-backend/controllers"
+	"noir-backend/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func adminRouter(r *gin.RouterGroup) {
+	r.Use(middleware.AuthMiddleware())
 	r.POST("/user")                                 //add admin
 	r.POST("/movie", controllers.AddMovie)          //add movie by admin
 	r.PATCH("/movie/:id", controllers.UpdateMovie)  //edit movie by admin
