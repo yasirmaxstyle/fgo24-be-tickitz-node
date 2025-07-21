d03383632ca91de837293522e154cf5dd55ef280
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-21 10:30:37 +0700

    feat: add showtimes model data types

---
+++ src/models/showtimes.js
@@ -0,0 +1,27 @@
+"use strict";
+const {
+  Model
+} = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Showtime extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Showtime.init({
+    movie_id: DataTypes.INTEGER,
+    cinema_id: DataTypes.INTEGER,
+    show_datetime: DataTypes.DATE,
+    price: DataTypes.DECIMAL,
+    available_seats: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: "Showtime",
+  });
+  return Showtime;
+};
