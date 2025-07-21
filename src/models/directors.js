64299ceac240d7929333138a9dbac3f9b7db3a40
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-21 10:28:28 +0700

    feat: add directors model data types

---
+++ src/models/directors.js
@@ -0,0 +1,42 @@
+"use strict";
+const {
+  Model
+} = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Director extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Director.hasMany(models.Movie, {
+        foreignKey: "director_id",
+        as: "movies"
+      });
+    }
+  }
+
+  Director.init({
+    director_id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: "Director",
+    tableName: "directors",
+    timestamps: false
+  });
+  return Director;
+};
