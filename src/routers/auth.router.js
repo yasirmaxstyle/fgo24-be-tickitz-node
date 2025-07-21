fad7ca98e442db35fa4928b48a73153c335c229f
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-21 10:34:01 +0700

    feat: add auth router endpoint

---
+++ src/routers/auth.router.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const AuthController = require("../controllers/auth.controller");
+const { authMiddleware } = require("../middlewares/auth.middleware");
+const {
+  registerValidation,
+  loginValidation,
+  forgotPasswordValidation,
+  resetPasswordValidation,
+  handleValidationErrors
+} = require("../middlewares/validations/authValidation");
+
+// Public routes
+router.post("/register", registerValidation, handleValidationErrors, AuthController.register);
+router.post("/login", loginValidation, handleValidationErrors, AuthController.login);
+router.post("/forgot-password", forgotPasswordValidation, handleValidationErrors, AuthController.forgotPassword);
+router.post("/reset-password", resetPasswordValidation, handleValidationErrors, AuthController.resetPassword);
+
+// Protected routes
+router.post("/logout", authMiddleware, AuthController.logout);
+router.get("/profile", authMiddleware, AuthController.getProfile);
+
+module.exports = router;
