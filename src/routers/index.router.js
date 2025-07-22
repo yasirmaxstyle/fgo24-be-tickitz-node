31f79ab47087ea7f976c2af9ff2aa9cee08201d3
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-22 09:24:58 +0700

    feat: add route admin, movie and transaction

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 
 router.use("/auth", require("./auth.router"));
 router.use("/profile", require("./profile.router"));
-// router.use("/admin", require("./admin.router"));
-// router.use("/movie", require("./movie.router"));
-// router.use("/transaction", require("./transaction.router"));
+router.use("/admin", require("./admin.router"));
+router.use("/movies", require("./movie.router"));
+router.use("/transaction", require("./transaction.router"));
 
 module.exports = router;
